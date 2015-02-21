a095973814d48c7562abef15849a34a5bf7081f8
edan <edanl@outlook.com>
2015-02-21 14:30:39 -0500

    Added user's location capability

--- FindATestingCenter/ViewController.h
+++ FindATestingCenter/ViewController.h
@@ -8,10 +8,12 @@
 
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <CLLocationManagerDelegate,MKMapViewDelegate>
 
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (strong, nonatomic) CLLocationManager *locationManager;
 
 @end
 
--- FindATestingCenter/ViewController.m
+++ FindATestingCenter/ViewController.m
@@ -17,21 +17,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self startLocationManager];
+    self.mapView.showsUserLocation = YES;
+}
+
+-(void)startLocationManager {
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    [self.locationManager requestAlwaysAuthorization];
+    self.locationManager.distanceFilter = kCLDistanceFilterNone; // whenever we move
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters; // 100 m
+    [self.locationManager startUpdatingLocation];
+    [self.locationManager requestWhenInUseAuthorization];
 }
 
 
+- (NSString *)deviceLocation {
+    return [NSString stringWithFormat:@"latitude: %f longitude: %f", self.locationManager.location.coordinate.latitude, self.locationManager.location.coordinate.longitude];
+}
+
 - (void)viewWillAppear:(BOOL)animated {
-    // 1
     CLLocationCoordinate2D zoomLocation;
     zoomLocation.latitude = 37.5333;
     zoomLocation.longitude= -77.4667;
-    
-    // 2
     MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, 5*METERS_PER_MILE, 5*METERS_PER_MILE);
+
+    [self.mapView setRegion:viewRegion animated:YES];
     
-    // 3
-    [_mapView setRegion:viewRegion animated:YES];
+    NSLog(@"%@", [self deviceLocation]);
 }
+
+
 
 @end
